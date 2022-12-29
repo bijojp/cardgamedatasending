6cbc8dbffbe7cb09e4f01d7a0d7196aa672bc074
Bijo <bijo.panicker@cuemath.com>
2022-12-29 15:07:00 +0530

    updated code

--- public/StartScene.js
+++ public/StartScene.js
@@ -62,60 +62,60 @@ class StartScene extends Phaser.Scene {
       const d = new Date();
       let time = d.getTime();
 
-      console.log("Version: " + "1.2");
+      console.log("Version: " + "1.3");
 
 
       var xhr = new XMLHttpRequest();
       var url = "https://expressserveronrailway-production-00c1.up.railway.app/postsomeotherdata";
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4 && xhr.status === 200) {
-              var json = JSON.parse(xhr.responseText);
-              // console.log(json.name + ", " + json.email+ ", " + json.id);
-              console.log("data posted");
-            console.log(xhr);
-          }
-      };
-      var data1 = JSON.stringify({"totalTime":RandomTime,"unixTime":time});
-      xhr.send(data1);
-
-          // }
+      // xhr.open("POST", url, true);
+      // xhr.setRequestHeader("Content-Type", "application/json");
+      // xhr.onreadystatechange = function () {
+      //     if (xhr.readyState === 4 && xhr.status === 200) {
+      //         var json = JSON.parse(xhr.responseText);
+      //         // console.log(json.name + ", " + json.email+ ", " + json.id);
+      //         console.log("data posted");
+      //       console.log(xhr);
+      //     }
       // };
-      // var data = JSON.stringify({"totalTime":RandomTime,"unixTime":time});
-      // xhr.send(data);
-      
 
+      
+      
+      xhr.onerror = function (event) {
+        retry(xhr)
+      }
+      xhr.onload = function (event) {
+        if (xhr.status < 200 || xhr.status >= 300)
+          retry(xhr)
+      }
+      xhr.ontimeout = function (event) {
+        retry(xhr)
+      }
+      openAndSend(xhr)
 
     });
 
 
+  }
 
+  retry(xhr) {
+    setTimeout(function () {
+      openAndSend(xhr)
+    }, 1000)
+  }
+  openAndSend(xhr) {
+    // xhr.open("GET", "http://example.com/")
 
-    // function retry(xhr) {
-    //   setTimeout(function () {
-    //     openAndSend(xhr)
-    //   }, 1000)
-    // }
-    // function openAndSend(xhr) {
-    //   // xhr.open("GET", "http://example.com/")
+    xhr.open("POST", "https://expressserveronrailway-production-00c1.up.railway.app/postsomeotherdata", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    // xhr.send()
 
-    //   xhr.open("POST", "https://expressserveronrailway-production-00c1.up.railway.app/postsomeotherdata", true);
-    //   xhr.setRequestHeader("Content-Type", "application/json");
-    //   // xhr.send()
+    // var RandomTime = Phaser.Math.Between(100000000, 10000000000);
 
-    //   // var RandomTime = Phaser.Math.Between(100000000, 10000000000);
+    // const d = new Date();
+    // let time = d.getTime();
 
-    //   // const d = new Date();
-    //   // let time = d.getTime();
-
-    //   var data1 = JSON.stringify({"totalTime":100000,"unixTime":121212});
-    //   xhr.send(data1);
-    // }
-
-
-
-
+    var data1 = JSON.stringify({"totalTime":1000,"unixTime":1010});
+    xhr.send(data1);   
   }
 
 
